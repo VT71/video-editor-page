9e54da96bc6256f02c5f51af38c5f52c33b95418
VT71 <56217276+VT71@users.noreply.github.com>
2023-11-10 02:23:30 +0000

    Loading progress added + gh-pages installed.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
+// @mui
+import { Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import LinearProgress, {
+    linearProgressClasses,
+} from '@mui/material/LinearProgress';
+
 // components
 import ArtSection from './ArtSection';
 import IntroSection from './IntroSection';
 import ContactSection from './ContactSection';
+
+const CustomLinearProgress = styled(LinearProgress)(() => ({
+    width: '250px',
+    height: '4px',
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+        backgroundColor: '#C924E9',
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+        backgroundColor: '#490061',
+    },
+}));
 
 function App() {
     const [loading, setIsLoading] = useState(true);
@@ -15,13 +33,47 @@ function App() {
     }, []);
 
     console.log('loading: ' + loading);
-    return (
-        <div className='App'>
-            <IntroSection />
-            <ArtSection />
-            <ContactSection />
-        </div>
-    );
+    if (loading) {
+        return (
+            <div className='App' style={{ position: 'relative' }}>
+                <IntroSection />
+                <ArtSection />
+                <ContactSection />
+                <div
+                    style={{
+                        width: '100%',
+                        height: '100vh',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        rowGap: '0.5rem',
+                        backgroundColor: '#0F0F0F',
+                        position: 'absolute',
+                        top: 0,
+                        zIndex: 100,
+                    }}
+                >
+                    <Typography
+                        sx={{
+                            fontSize: '1rem',
+                            fontFamily: 'Goldman-Regular',
+                            color: '#E0C8E8',
+                        }}
+                    >{`Loading`}</Typography>
+                    <CustomLinearProgress />
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className='App'>
+                <IntroSection />
+                <ArtSection />
+                <ContactSection />
+            </div>
+        );
+    }
 }
 
 export default App;
